07f35e4859b02c35de22593b0a6caa3b40231062
HarisImtiaz2022skipq <haris.imtiaz.skipq@gmail.com>
2024-11-12 01:17:48 +0500

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import WeatherPage from "./weather/page";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return <>
+  <h1 className="text-2xl text-center mt-4">Weather Forecast</h1>
+  <WeatherPage/>
+  </>
+
 }
